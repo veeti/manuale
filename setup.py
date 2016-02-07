d4bd537bd6f23b35f4d90a2bcbf50b9e38878953
Veeti Paananen <veeti.paananen@rojekti.fi>
2016-02-07 23:53:44 +0200

    1.0.1.dev0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='manuale',
-    version='1.0.0',
+    version='1.0.1.dev0',
 
     license='MIT',
     description="A fully manual Let's Encrypt/ACME client",
