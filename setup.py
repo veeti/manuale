a8163b0da1da0c303f4b7427473360ddddf982e8
Veeti Paananen <veeti.paananen@rojekti.fi>
2016-02-08 00:10:08 +0200

    Convert the README to rst if pandoc is installed

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import subprocess
 from setuptools import setup
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    readme = f.read()
+readme = path.join(here, 'README.md')
+
+# Convert the README to reStructuredText for PyPI if pandoc is available.
+# Otherwise, just read it.
+try:
+    readme = subprocess.check_output(['pandoc', '-f', 'markdown', '-t', 'rst', readme]).decode('utf-8')
+except:
+    with open(readme, encoding='utf-8') as f:
+        readme = f.read()
 
 setup(
     name='manuale',
